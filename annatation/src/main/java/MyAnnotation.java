8c33ed616f987e776369f909877150acc39d311c
bq <bq7327645@163.com>
2020-09-25 18:14:57 +0800

    annotation

---
+++ annatation/src/main/java/MyAnnotation.java
@@ -0,0 +1,10 @@
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Annotation在反射函数中的使用示例
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@interface MyAnnotation {
+    String[] value() default "unknown";
+}
